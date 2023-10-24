de6f0edd0148df916ff81573e1b618347dd0c0a7
Nanami Kimoto <barrier-officer0z@icloud.com>
2023-10-23 23:16:39 -0700

    output for java

--- Java-Primer/Output.java
+++ Java-Primer/Output.java
@@ -7,10 +7,10 @@ The main method is where execution begins.
 
 public class Output {
     /*
-    The 'public' modifier in a class or a mathod determines its accessibility.
+    The 'public' modifier in a class or a method determines its accessibility.
     The 'static' keyword means the method belongs to the class.
     The 'void' keyword means that the main method doesn't return a value.
-    The 'main' keyword means 'main method' and is typical for msot programming languages.
+    The 'main' keyword means 'main method' and is typical for most programming languages.
     The 'String[]' keyword represents an array of strings that you can pass command-line arguments to.
     The 'args' keyword represents an array of strings that can hold arguments for your program.
      */
